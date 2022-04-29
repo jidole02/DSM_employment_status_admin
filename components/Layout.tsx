106e329116b22530ee3f3779c26eef35dbaa0e88
jiwon <jidole041214@naver.com>
2022-04-29 11:07:10 +0900

    create 레이아웃

---
+++ components/Layout.tsx
@@ -0,0 +1,30 @@
+import React, { ReactNode } from "react";
+import styled from "@emotion/styled";
+
+interface Props {
+  children: ReactNode;
+}
+
+function Layout({ children }: Props) {
+  return (
+    <Base>
+      <Container>{children}</Container>
+    </Base>
+  );
+}
+
+export default Layout;
+
+const Base = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+`;
+
+const Container = styled.article`
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+`;
